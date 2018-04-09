edec33eb08212056e01ee2c1044faeb8d9f12216
Sung J Byun <sungjbyun@D-108-179-186-108.dhcp4.washington.edu>
2018-04-08 21:23:06 -0700

    work on Mutations

---
+++ imports/ui/ResolutionForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+
+export default class ResolutionForm extends Component {
+  submitForm = () => {
+    console.log(this.name.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type='text' ref={(input) => (this.name = input)} />
+        <button onClick={this.submitForm}>Submit</button>
+      </div>
+    )
+  }
+}
